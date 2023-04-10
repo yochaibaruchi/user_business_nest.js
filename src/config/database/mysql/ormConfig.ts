3a92e4801d4c272b4250d443a05c15181a635654
yochai baruchi <yochaybaruchi@gmail.com>
2023-04-10 15:48:35 +0300

    mogration

---
+++ src/config/database/mysql/ormConfig.ts
@@ -0,0 +1,31 @@
+
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { MysqlConfigService } from './config.service';
+import { User } from '../../../user/user.entity';
+import * as dotenv from 'dotenv';
+
+
+
+dotenv.config();
+ 
+const configService = new ConfigService(process.env);
+const mysqlConfigService = new MysqlConfigService(configService);
+ 
+export default new DataSource({
+  type: 'mysql',
+  host: mysqlConfigService.host,
+  port: mysqlConfigService.port,
+  username: mysqlConfigService.username,
+  password: mysqlConfigService.password,
+  database: mysqlConfigService.database,
+  entities: [User],
+  migrations:  [],
+});
+
+
+
+
+
+
+
