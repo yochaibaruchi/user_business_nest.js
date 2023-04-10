41ee2124081e2a5141f9e8b53fe5fe64adc37b58
yochai baruchi <yochaybaruchi@gmail.com>
2023-04-10 13:23:32 +0300

    jwt updte

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiBody, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, UseGuards, Res , Req, Get, InternalServerErrorException} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiBody, ApiTags, ApiBearerAuth,ApiUnauthorizedResponse, ApiOkResponse} from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './local-auth.guard';
+
+import { Response  } from 'express';
+import { ILoginReq } from './auth.interfaces';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { RefreshTokenGuard } from './guards/refresh-token.guard';
+import { JwtAuthGuard } from './guards/jwt.auth.guard';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -39,7 +44,62 @@ export class AuthController {
     },
   })
   // SWAGGER
-  async login(@Request() req): Promise<{ access_token: string, refresh_token: string }>{
-    return this.authService.login(req.user);
+  async login(@Req() req: ILoginReq, @Res({ passthrough: true }) res: Response): Promise<{ access_token: string }> {
+    try {
+      const tokens = await this.authService.login(req.user);
+  
+      // Set the refresh token as an HTTP-only cookie
+      res.cookie('refreshToken', tokens.refresh_token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000
+        // Add additional options like `secure: true` and `sameSite` if needed
+      });
+      return { access_token: tokens.access_token };
+    } catch (error) {
+      throw new InternalServerErrorException('An error occurred while logging in');
+    }
   }
+
+
+
+@UseGuards(RefreshTokenGuard)
+  @Get('refresh')
+@ApiOperation({ summary: 'Checks if the refresh token is valid and returns new tokens if true.' })
+@ApiOkResponse({ description: 'Successfully generated new access token', type: String })
+@ApiUnauthorizedResponse({ description: 'Invalid or expired refresh token' })
+async refresh(@Req() req : ILoginReq  ,@Res({passthrough: true}) res : Response ) : Promise<{ access_token: string}> {
+
+console.log(req.user +" controller");
+
+  const tokens = await this.authService.login(req.user);
+
+
+  // Set the refresh token as an HTTP-only cookie
+  res.cookie('refreshToken', tokens.refresh_token, {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000
+    // Add additional options like `secure: true` and `sameSite` if needed
+  });
+  return {access_token : tokens.access_token}
+
+}
+
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@Post('logout')
+@ApiOperation({ summary: 'Logout user' })
+@ApiResponse({ status: 200, description: 'User logged out successfully' })
+async logout(@Req() req: ILoginReq, @Res({passthrough: true}) res : Response): Promise<void> {
+  // Get the user ID from the JWT payload
+  const userId = req.user.id;
+
+
+  // Call the logout method to remove the refresh token from the database
+  await this.authService.logout(userId);
+
+  // Instruct the client to delete the refresh token from the cookie
+  res.clearCookie('refreshToken')
+  res.status(200).send({ message: 'Logged out successfully' });
+}
+
 }
--- src/auth/guards/jwt.auth.guard.ts
+++ src/auth/guards/jwt.auth.guard.ts
@@ -1,1 +1,7 @@
-//doto
+
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {}
+// working well DOTO: using the guard in all routs that need it.
