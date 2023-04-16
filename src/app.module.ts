356872add908d3bd41ba473d9655770923ff4ce7
yochai baruchi <yochaybaruchi@gmail.com>
2023-04-16 14:16:11 +0300

    added role entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,9 @@ import { HealthCheckController } from './health-check/health-check.controller';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { BusinessModule } from './business/business.module';
+import { RoleModule } from './role/role.module';
 @Module({
-  imports: [MysqlDatabaseProviderModule, TerminusModule,UserModule ,AuthModule , BusinessModule],
+  imports: [MysqlDatabaseProviderModule, TerminusModule,UserModule ,AuthModule , BusinessModule, RoleModule],
   controllers: [AppController, HealthCheckController],
   providers: [AppService],
 })
